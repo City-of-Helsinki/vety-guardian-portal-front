1bd5d059ddc243f9e00b7bd4f4fdee53e93d046e
Henri Aho <henri.aho@codemen.fi>
2026-09-01 17:40:27 +0300

    Config, packages, i18n, layout sketch

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,42 @@
-import './App.css'
+import './App.css';
+import { useTranslation } from 'react-i18next';
+import { Footer, Header, Logo, logoFi, logoSv } from 'hds-react';
 
 function App() {
+  const { t, i18n } = useTranslation();
+
+  const changeLanguage = (lang: string) => {
+    i18n.changeLanguage(lang);
+  };
 
   return (
     <>
-      <section id="center">
-        <h1>Hello World!</h1>
+      <Header
+        languages={[
+          { label: 'Suomi', value: 'fi', isPrimary: true },
+          { label: 'Svenska', value: 'sv', isPrimary: true },
+          { label: 'English', value: 'en', isPrimary: true },
+        ]}
+        onDidChangeLanguage={(newLanguage) => changeLanguage(newLanguage)}
+      >
+        <Header.ActionBar
+          className=""
+          frontPageLabel="test"
+          title="test"
+          titleHref="#"
+          logo={
+            <Logo src={i18n.language === 'sv' ? logoSv : logoFi} alt="asd" />
+          }
+        >
+          <Header.LanguageSelector />
+        </Header.ActionBar>
+      </Header>
+      <section style={{ flexGrow: 1 }}>
+        <h1>{t('title')}</h1>
       </section>
+      <Footer></Footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/i18n/i18n.ts
@@ -0,0 +1,31 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import fi from './locales/fi.json';
+import en from './locales/en.json';
+import sv from './locales/sv.json';
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources: {
+      fi: {
+        translation: fi,
+      },
+      en: {
+        translation: en,
+      },
+      sv: {
+        translation: sv,
+      },
+    },
+    debug: true,
+    fallbackLng: 'fi',
+    supportedLngs: ['fi', 'en', 'sv'],
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default i18n;
