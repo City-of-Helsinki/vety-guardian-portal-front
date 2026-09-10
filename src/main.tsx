b0932748cd4a65ae39adbd8c17c9fa77cae86b62
Mikko Niemela <mikko.niemela@codemen.fi>
2026-09-10 17:13:26 +0300

    feat: set query client

--- src/main.tsx
+++ src/main.tsx
@@ -6,17 +6,22 @@ import App from './App.tsx';
 import { Home, Landing, Application } from './pages';
 import './i18n/i18n';
 import './api/client.ts';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route index element={<Home />} />
-          <Route path="landing" element={<Landing />} />
-          <Route path="application" element={<Application />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route index element={<Home />} />
+            <Route path="landing" element={<Landing />} />
+            <Route path="application" element={<Application />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>,
 );
